4c053cf7b8055b37efd2759f2f8551bb955aa48b
seventhsense <seventhsense@users.noreply.github.com>
2015-03-29 10:47:50 +0900

    Update init.rb
    change url to github.com

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_japanese_user_format do
   author 'Seventh'
   description 'ユーザー用のフォームと一覧を、苗字、名前の順番にします.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/seventhsense/redmine_japanese_user_format'
   author_url 'http://blog.scimpr.com'
 end
