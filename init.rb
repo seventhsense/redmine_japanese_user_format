5042977f3ac623b6df52ddbf7c63efc4ddb64e92
seventhsense <seventh@scimpr.com>
2015-03-29 10:40:48 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_japanese_user_format do
+  name 'Redmine Japanese User Format plugin'
+  author 'Seventh'
+  description 'ユーザー用のフォームと一覧を、苗字、名前の順番にします.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://blog.scimpr.com'
+end
